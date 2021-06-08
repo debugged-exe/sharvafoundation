3ebd8f8a2763f53423f2b87e822cc18996949f22
sumedhovhal <sumedhovhal765@gmail.com>
2021-06-08 12:37:16 +0530

    navbar done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,48 +4,72 @@ import {Link} from "react-router-dom";
 import imgUrl from './sharva.png';
 import Aboutus from '../Aboutus/Aboutus';
 
-function Navbar () {
+const Navbar = ()  => {
 
-  const [navbarContents,setNavbarContents]=useState(true);
+   const [navbarContents,setNavbarContents]=useState('navbarContents');
+  const[dropDown,setDropDown]=useState('hidden');
+  const [line, setLine]  = useState('show');
+    const hamburger = () => {
+        if (dropDown==='hidden'){
+            setDropDown('visible')
+            setNavbarContents('navbarContents-resp')
+            setLine('hide')
+        }
+        else{
+            setDropDown('hidden');
+            setNavbarContents('navbarContents')
+            setLine('show')
+        }
+    }
 
     return(
-      <nav className="flex justify-even items-center ">
+
+      <nav className={`flex justify-between items-center ${navbarContents}`}>
         <div className="mr2">
           <img src={imgUrl} alt="Logo" style={{height:"60px",width:"60px"}}></img>
         </div>
 
+        <div className={`hamburger ${line} pr2`}
+                  onClick={()=>hamburger()}
+                  >
+                          <div className='line'></div>
+                          <div className='line'></div>
+                          <div className='line'></div>
+        </div>
 
-        <div className= {`hide-contents ${navbarContents?'show':'hide'}`}  >
 
-          <ul className="list-style-none flex justify-around pa2 mr2 items-center ">
+        <div className=  {` ${dropDown}`}  >
+
+          <ul className={` ${navbarContents} list-style-none flex justify-around pa2 mr2 items-center ${dropDown} `}>
 
             <Link to="/">
-                <li className="mh2 f5 nav-item ">Home</li></Link>
+                <li className="mh2  nav-item">Home</li></Link>
             <Link to="/">
-                <li className="mh2 f5 nav-item"><a href="/">Our Initiatives</a></li></Link>
+                <li className="mh2  nav-item pr2 ">Our Initiatives</li></Link>
             <Link to="/">
-                <li className="mh f5 nav-item"><a href="/">Covid 19</a></li></Link>
+                <li className="mh  nav-item pl1">Covid 19</li></Link>
             <Link to="/" >
-                <li className="mh2 f5 nav-item"><a href="/">About Us</a></li></Link>
+                <li className="mh2  nav-item pr1 pl1">About Us</li></Link>
             <Link to="/">
-            <li className="mh2 f5 nav-item"><a href="/">Join Us</a></li></Link>
+            <li className="mh2  nav-item">Join Us</li></Link>
             <Link to="/contactus">
-                <li className="mh2 f5 nav-item">Contact Us</li></Link>
-                
+                <li className="mh2  nav-item pr1">Contact Us</li></Link>
+                <div className="pt2 pb3">
+              <a class="f5 link dim br2 ph3 pv2 white nav-donate " href="" style={{backgroundColor:"#e88f0a"}}>Donate Now</a></div>
+
+              <p className='tc'><Link className={`f6 link dim ph3 pv1 mb2 dib white  cross flex justify-center items-center`} href='#0'
+                onClick={()=>hamburger()}
+                >
+                X
+               </Link>
+              </p>
+
+
           </ul>
         </div>
 
-
-        <div className= {` ml2 hide-contents ${navbarContents?'show':'hide'}`} >
-        <a class="f6 link dim br2 ph3 pv2 white nav-donate" href="" style={{backgroundColor:"#e88f0a"}}>Donate Now</a>
-        </div>
-        <div className="hamburger " onClick={()=>setNavbarContents(true)}>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
-        
       </nav>
+
     );
 }
 
