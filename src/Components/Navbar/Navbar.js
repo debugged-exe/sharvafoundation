dcd0b23ed5d98de4309bdaf2cecbd0078b8c9fdb
nikhilk-19 <nikhil.akhule1903@gmail.com>
2021-06-10 16:24:38 +0530

    Our initiatives dropdown and Card style changes added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,76 +1,84 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import imgUrl from './sharva.png';
 import Aboutus from '../Aboutus/Aboutus';
 
-const Navbar = ()  => {
+const Navbar = () => {
 
-   const [navbarContents,setNavbarContents]=useState('navbarContents');
-  const[dropDown,setDropDown]=useState('hidden');
-  const [line, setLine]  = useState('show');
-    const hamburger = () => {
-        if (dropDown==='hidden'){
-            setDropDown('visible')
-            setNavbarContents('navbarContents-resp')
-            setLine('hide')
-        }
-        else{
-            setDropDown('hidden');
-            setNavbarContents('navbarContents')
-            setLine('show')
-        }
+  const [navbarContents, setNavbarContents] = useState('navbarContents');
+  const [dropDown, setDropDown] = useState('hidden');
+  const [line, setLine] = useState('show');
+  const hamburger = () => {
+    if (dropDown === 'hidden') {
+      setDropDown('visible')
+      setNavbarContents('navbarContents-resp')
+      setLine('hide')
     }
+    else {
+      setDropDown('hidden');
+      setNavbarContents('navbarContents')
+      setLine('show')
+    }
+  }
 
-    return(
+  return (
 
-      <nav className={`flex justify-between items-center ${navbarContents}`}>
-        <div className="mr2">
-          <img src={imgUrl} alt="Logo" style={{height:"60px",width:"60px"}}></img>
-        </div>
+    <nav className={`flex justify-between items-center ${navbarContents}`}>
+      <div className="mr2">
+        <img src={imgUrl} alt="Logo" style={{ height: "60px", width: "60px" }}></img>
+      </div>
 
-        <div className={`hamburger ${line} pr2`}
-                  onClick={()=>hamburger()}
-                  >
-                          <div className='line'></div>
-                          <div className='line'></div>
-                          <div className='line'></div>
-        </div>
+      <div className={`hamburger ${line} pr2`}
+        onClick={() => hamburger()}
+      >
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
 
 
-        <div className=  {` ${dropDown}`}  >
+      <div className={` ${dropDown}`}  >
 
-          <ul className={` ${navbarContents} list-style-none flex justify-around pa2 mr2 items-center ${dropDown} `}>
+        <ul className={` ${navbarContents} list-style-none flex justify-around pa2 mr2 items-center ${dropDown} `}>
 
-            <Link to="/">
-                <li className="mh2 black nav-item">Home</li></Link>
-            <Link to="/">
-                <li className="mh2 black nav-item pr2 ">Our Initiatives</li></Link>
-            <Link to="/">
-                <li className="mh black nav-item pl1">Covid 19</li></Link>
-            <Link to="/" >
-                <li className="mh2 black nav-item pr1 pl1">About Us</li></Link>
-              <Link to="/joinus">
-            <li className="mh2  black nav-item">Join Us</li></Link>
-            <Link to="/contactus">
-                <li className="mh2 black nav-item pr1">Contact Us</li></Link>
-                <div className="pt2 pb3">
-              <a class="f5 link dim br2 ph3 pv2 white nav-donate " href="" style={{backgroundColor:"#e88f0a"}}>Donate Now</a></div>
+          <Link to="/">
+            <li className="list-style-none mh2 black menu">Home</li></Link>
+          <Link to="/">
+            <li className="list-style-none mh2 black menu">Our Initiatives
+            <ul className="sub-menu black align-center">
+                <li className="sub-menu-item"><a>HungerFreeIndia</a></li>
+                <li className="sub-menu-item"><a>StriveWithPride</a></li>
+                <li className="sub-menu-item"><a>ProjectRuya</a></li>
+                <li className="sub-menu-item"><a>AsFreeAsBird</a></li>
+                <li className="sub-menu-item"><a>GiveHerWingsToFly</a></li>
+              </ul>
+            </li></Link>
+          <Link to="/">
+            <li className="list-style-none mh2 black menu">Covid 19</li></Link>
+          <Link to="/" >
+            <li className="list-style-none mh2 black menu">About Us</li></Link>
+          <Link to="/joinus">
+            <li className="list-style-none mh2 black menu">Join Us</li></Link>
+          <Link to="/contactus">
+            <li className="list-style-none mh2 black menu">Contact Us</li></Link>
+          <div className="pv2">
+            <a class="f5 link dim br2 ph3 pv2 white nav-donate" href="" style={{ backgroundColor: "#e88f0a" }}>Donate Now</a></div>
 
-            <p className='tc'><Link className={`f6 link dim ph3 pv1 mb2 dib white crossNavbar flex justify-center items-center`} href='#0'
-                onClick={()=>hamburger()}
-                >
-                X
+          <p className='tc'><Link className={`f6 link dim ph3 pv1 mb2 dib white crossNavbar flex justify-center items-center`} href='#0'
+            onClick={() => hamburger()}
+          >
+            X
                </Link>
-              </p>
+          </p>
 
 
-          </ul>
-        </div>
+        </ul>
+      </div>
 
-      </nav>
+    </nav >
 
-    );
+  );
 }
 
 export default Navbar;
--- src/Components/OurGoals/Card.js
+++ src/Components/OurGoals/Card.js
@@ -2,28 +2,32 @@ import React from 'react';
 import './Card.css';
 import one from './one.jpeg';
 
-function Card(){
-    return(
+function Card() {
+    return (
         <div className="newcard">
             <div className="cardTop">
-                <img src={one} className="cardImg"/>
+                <img src={one} className="cardImg" />
             </div>
-            <div className="cardBottom flex-column justify-center items-center">
+            <div className="cardBottom flex-column justify-center items-center h-100">
                 <div className="Detail">
-                <h4> Event Heading which here is used as a sample template Heading </h4>
-              <p><b>₹42,000</b> raised out of  ₹60,000</p>
+                    <h4> Event Heading which here is used as a sample template Heading </h4>
+                    <p><b>₹42,000</b> raised out of  ₹60,000</p>
                     <div class="skill-bar">
-                         <div class="skill-per" per="90"></div>
+                        <div class="skill-per" per="90"></div>
                     </div>
                 </div>
                 <div className="flex justify-center items-center">
-                        <a className="f5  link br2 ph3 pv2 mb2 tc  dib cardButton" href="#0">Donate Now</a>
+                    <a className="f5  link br2 ph3 pv2 mb2 tc  dib cardButton" href="#0">Donate Now</a>
+                </div>
+                <div className="lastLine">
+                    <div className="supporters">
+                        <p><b>100 Supporters</b></p>
                     </div>
-                <div className="lastLine ">
-                    <p><b>100 Supporters</b></p>
-                    <a href="sharvafoundation.org" className="">
-                        <b className="LearnColor">Learn More</b>
-                    </a>
+                    <div>
+                        <a href="sharvafoundation.org" className="">
+                            <b className="LearnColor">Learn More</b>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
