c5e465a8d0345d3676be68909ecd49fe0c5fb938
sumedhovhal <sumedhovhal765@gmail.com>
2021-06-25 23:34:19 +0530

    final frontend

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,36 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Counter.css';
-import { GiMeal } from 'react-icons/gi';
-import { FaHandsHelping } from 'react-icons/fa';
-import { GiAngelWings } from 'react-icons/gi';
-import { FaRegIdCard } from 'react-icons/fa';
+import {GiMeal} from 'react-icons/gi';
+import {FaHandsHelping} from 'react-icons/fa';
+import {GiAngelWings} from 'react-icons/gi';
+import {FaRegIdCard} from 'react-icons/fa';
 import CountUp from 'react-countup';
 
 function Counter() {
-    return (
-        <div className="counter-container bg-white">
-            <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-                <GiMeal color="#FF9800" size="3em" />
-            <div className="pv2"><span className="txt num1"><CountUp end={100} duration={20}></CountUp></span></div>
-                <div className="pv2"><span className="txt">Food Kits</span></div>
-            </div>
-            <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-                <FaHandsHelping color="#8BCA4E" size="3em" />
-                <div className="pv2"><span className="txt num2"><CountUp end={200} duration={20}></CountUp></span></div>
-                <div className="pv2"><span className="txt">People Helped</span></div>
-            </div>
-            <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-                <GiAngelWings color="#FF9800" size="3em" />
-                <div className="pv2"><span className="txt num1"><CountUp end={300} duration={20}></CountUp></span></div>
-                <div className="pv2"><span className="txt">Sanitary</span></div>
-            </div>
-            <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-                <FaRegIdCard color="#FF9800" size="3em" />
-                <div className="pv2"><span className="txt num2"><CountUp end={400} duration={20}></CountUp></span></div>
-                <div className="pv2"><span className="txt">Volunteers</span></div>
-            </div>
-        </div>
-    )
+  const [count, setCount] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/").then(response => response.json()).then(res => {
+      if (res[0].name) {
+        setCount(res);
+        console.log(res[0].name);
+      }
+    }).catch(error => {
+      console.log(error);
+    })
+
+  },[])
+  
+  return (<div className="counter-container bg-white">
+      {
+        count.map((item, index) => {
+          if(index===0 || (index%2)===0){
+            return (<div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+              <GiMeal color="#FF9800" size="3em"/>
+              <div className="pv2">
+                <span className="txt num1">
+                  <CountUp end={item.count} duration={10}></CountUp>
+                </span>
+                <div className="pv2"><span className="txt">{item.name}</span></div>
+              </div>
+            </div>)
+          }
+          else if(index===1 ||(index%2)!==0){
+            return (<div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+              <GiMeal color="#8BCA4E" size="3em"/>
+              <div className="pv2">
+                <span className="txt num1" style={{color:"#8BCA4E"}}>
+                  <CountUp end={item.count} duration={10}></CountUp>
+                </span>
+                <div className="pv2"><span className="txt">{item.name}</span></div>
+              </div>
+            </div>)
+          }
+
+      })
+    }
+  </div>)
 }
 
 export default Counter;
+// {/* <div className="counter-container bg-white">
+//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+//         <GiMeal color="#FF9800" size="3em" />
+//     <div className="pv2"><span className="txt num1"><CountUp end={100} duration={20}></CountUp></span></div>
+//         <div className="pv2"><span className="txt">Food Kits</span></div>
+//     </div>
+//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+//         <FaHandsHelping color="#8BCA4E" size="3em" />
+//         <div className="pv2"><span className="txt num2"><CountUp end={200} duration={20}></CountUp></span></div>
+//         <div className="pv2"><span className="txt">People Helped</span></div>
+//     </div>
+//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+//         <GiAngelWings color="#FF9800" size="3em" />
+//         <div className="pv2"><span className="txt num1"><CountUp end={300} duration={20}></CountUp></span></div>
+//         <div className="pv2"><span className="txt">Sanitary</span></div>
+//     </div>
+//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
+//         <FaRegIdCard color="#FF9800" size="3em" />
+//         <div className="pv2"><span className="txt num2"><CountUp end={400} duration={20}></CountUp></span></div>
+//         <div className="pv2"><span className="txt">Volunteers</span></div>
+//     </div>
+// </div> */}
