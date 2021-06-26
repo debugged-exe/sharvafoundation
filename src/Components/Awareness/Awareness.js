49c50631bebfa8d338a971aee81f8618fa2f67fe
sumedhovhal <sumedhovhal765@gmail.com>
2021-06-26 16:03:17 +0530

    awarenesspage connected to backend

--- src/Components/Awareness/Awareness.js
+++ src/Components/Awareness/Awareness.js
@@ -1,111 +1,136 @@
-import React from 'react';
-//css
+import React, {useState, useEffect} from 'react';
 import './Awareness.css';
 import Carousel from 'react-elastic-carousel';
 import BackgroundCarousel from '../BackgroundCarousel/BackgroundCarousel';
 import pic1 from "../../Database/1.jpeg";
 
 const breakPoints = [
-    { showArrows: false },
-
+  {
+    showArrows: false
+  }
 ]
-
 function Awareness() {
-    return (
-        <div className="awareness-body">
 
-            <Carousel enableAutoPlay={true} autoPlaySpeed={2000} breakPoints={breakPoints}>
-                <div className='slide1 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide2 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide3 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide1 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"}>
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide2 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide3 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide1 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide2 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-                <div className='slide3 flex justify-center items-center font-size flex-column'>
-                    <div data-aos="fade-up" data-aos-duration={"1000"} >
-                        <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
-                    </div>
-                </div>
-            </Carousel>
+  const [aware, setAware] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/awareness")
+    .then(response => response.json())
+    .then(res => {
+      if (res[0].heading) {
+        setAware(res);
+        console.log(res[0].heading);
+      }
+    }).catch(error => {
+      console.log(error);
+    })
 
+  }, [])
 
-            <div className="awareness-card">
+  return (
+
+    <div className="awareness-body">
+
+    <Carousel enableAutoPlay={true} autoPlaySpeed={2000} breakPoints={breakPoints}>
+      <div className='slide1 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide2 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide3 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide1 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide2 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide3 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide1 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide2 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+      <div className='slide3 flex justify-center items-center font-size flex-column'>
+        <div data-aos="fade-up" data-aos-duration={"1000"}>
+          <h3 className="tagline">लोका: समस्ता: सुखिनो भवन्तु ।</h3>
+        </div>
+      </div>
+    </Carousel>
+
+    <div>
+      {
+        aware.map((item, index) =>
+        {
+          if(index===0 || (index)%2===0){
+            return(
+
+              <div className="awareness-card">
+                <div className="awareness-card-img">
+                  <img src={item.img} alt="Alt text"/>
+                </div>
                 <div className="awareness-card-text">
-                    <h1>Top heading</h1>
-                    <h4>Date</h4>
-                    <h4>Time</h4>
-                    <h4>Platform</h4>
-                    <h4>Speaker</h4>
-                    <p className="description">Description</p>
-                    <a className="f5 link dim br2 ph3 pv2 white nav-donate" href="/" style={{ backgroundColor: "#e88f0a" }} >Register Now</a>
+                  <h1>{item.heading}</h1>
+                  <h4>{item.date}</h4>
+                  <h4>{item.time}</h4>
+                  <h4>{item.platform}</h4>
+                  <h4>{item.speaker}</h4>
+                  <p className="description">{item.desp}</p>
+                  <a className="f5 link dim br2 ph3 pv2 white nav-donate" href="/" style={{
+                      backgroundColor: "#e88f0a"
+                    }}>Register Now</a>
+                </div>
+              </div>
+            )
+
+          }else if (index===1 || (index)%2!==0){
+            return(
+
+              <div className="awareness-card">
+
+                <div className="awareness-card-text">
+                  <h1>{item.heading}</h1>
+                  <h4>{item.date}</h4>
+                  <h4>{item.time}</h4>
+                  <h4>{item.platform}</h4>
+                  <h4>{item.speaker}</h4>
+                  <p className="description">{item.desp}</p>
+                  <a className="f5 link dim br2 ph3 pv2 white nav-donate" href="/" style={{
+                      backgroundColor: "#e88f0a"
+                    }}>Register Now</a>
                 </div>
                 <div className="awareness-card-img">
-                    <img src={pic1} alt="Alt text" />
+                  <img src={item.img} alt="Alt text"/>
                 </div>
-            </div>
-            <div className="awareness-card">
-                <div className="awareness-card-img">
-                    <img src={pic1} alt="Alt text" />
-                </div>
-                <div className="awareness-card-text">
-                    <h1>Top heading</h1>
-                    <h4>Date</h4>
-                    <h4>Time</h4>
-                    <h4>Platform</h4>
-                    <h4>Speaker</h4>
-                    <p className="description">Description</p>
-                    <a className="f5 link dim br2 ph3 pv2 white nav-donate" href="/" style={{ backgroundColor: "#e88f0a" }} >Register Now</a>
-                </div>
-            </div>
-            <div className="awareness-card">
-                <div className="awareness-card-text">
-                    <h1>Top heading</h1>
-                    <h4>Date</h4>
-                    <h4>Time</h4>
-                    <h4>Platform</h4>
-                    <h4>Speaker</h4>
-                    <p className="description">Description</p>
-                    <a className="f5 link dim br2 ph3 pv2 white nav-donate" href="/" style={{ backgroundColor: "#e88f0a" }} >Register Now</a>
-                </div>
-                <div className="awareness-card-img">
-                    <img src={pic1} alt="Alt text" />
-                </div>
-            </div>
-        </div>)
+              </div>
+            )
+          }
+
+        })
+      }
+    </div>
+  </div>
+)
 }
 
 export default Awareness;
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -19,7 +19,7 @@ function Counter() {
     })
 
   },[])
-  
+
   return (<div className="counter-container bg-white">
       {
         count.map((item, index) => {
@@ -52,25 +52,3 @@ function Counter() {
 }
 
 export default Counter;
-// {/* <div className="counter-container bg-white">
-//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-//         <GiMeal color="#FF9800" size="3em" />
-//     <div className="pv2"><span className="txt num1"><CountUp end={100} duration={20}></CountUp></span></div>
-//         <div className="pv2"><span className="txt">Food Kits</span></div>
-//     </div>
-//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-//         <FaHandsHelping color="#8BCA4E" size="3em" />
-//         <div className="pv2"><span className="txt num2"><CountUp end={200} duration={20}></CountUp></span></div>
-//         <div className="pv2"><span className="txt">People Helped</span></div>
-//     </div>
-//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-//         <GiAngelWings color="#FF9800" size="3em" />
-//         <div className="pv2"><span className="txt num1"><CountUp end={300} duration={20}></CountUp></span></div>
-//         <div className="pv2"><span className="txt">Sanitary</span></div>
-//     </div>
-//     <div className="floatCard grow pointer flex-column justify-center bg-white shadow-2 mb2 br3 tc mh4 pv4 ph4">
-//         <FaRegIdCard color="#FF9800" size="3em" />
-//         <div className="pv2"><span className="txt num2"><CountUp end={400} duration={20}></CountUp></span></div>
-//         <div className="pv2"><span className="txt">Volunteers</span></div>
-//     </div>
-// </div> */}
