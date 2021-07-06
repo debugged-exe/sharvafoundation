bc7b5432f68e9fc5a1a2bc0714245636a294ce5b
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-07-06 20:49:26 +0530

    added onclick on navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -49,30 +49,30 @@ const Navbar = ({ setInitiative }) => {
 
         <ul className={` ${navbarContents} list-style-none flex justify-around pa2 mr2 items-center ${dropDown} `}>
 
-          <Link to="/" ><li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/'?'acName':''}`}>Home</span></li></Link>
+          <Link to="/" ><li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/'?'acName':''}`}  onClick={() => hamburger()}>Home</span></li></Link>
 
         <Link to="/events" className="linkInitiatives">
             <li className="list-style-none mh2 black menu"><span className={` ${(location.pathname==='/events')&&(covid===false)?'acName':''}`}>Our Initiatives</span>
               <ul className="sub-menu black align-center">
-                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("hungerfreeindia");setCovid(false)}}> Hunger Free India</li></Link>
-                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("strivewithpride");setCovid(false)}}> Strive With Pride</li></Link>
-                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("projectruya");setCovid(false);setCovid(false)}}> Project Ruya</li></Link>
-                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("asfreeasabird");setCovid(false)}}> As Free As a Bird</li></Link>
-                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("giveherwings");setCovid(false)}}> Give Her Wings</li></Link>
+                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("hungerfreeindia");setCovid(false);hamburger()}} > Hunger Free India</li></Link>
+                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("strivewithpride");setCovid(false);hamburger()}} > Strive With Pride</li></Link>
+                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("projectruya");setCovid(false);setCovid(false);hamburger()}}> Project Ruya</li></Link>
+                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("asfreeasabird");setCovid(false);hamburger()}} > As Free As a Bird</li></Link>
+                <Link to="/events" className="linkInitiatives"><li className="sub-menu-item" onClick={() => {setInitiative("giveherwings");setCovid(false);hamburger()}} > Give Her Wings</li></Link>
               </ul>
             </li></Link>
           <Link to="/events">
-            <li className="list-style-none mh2 black menu" onClick={() => {setInitiative("covid19");setCovid(true)}}><span className={` ${location.pathname==="/events" && (covid===true)?'acName':''}`}>Covid 19</span></li></Link>
+            <li className="list-style-none mh2 black menu" onClick={() => {setInitiative("covid19");setCovid(true)}}><span className={` ${location.pathname==="/events" && (covid===true)?'acName':''}`}  onClick={() => hamburger()}>Covid 19</span></li></Link>
           <Link to="/awareness">
-          <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/awareness'?'acName':''}`}>Awareness</span></li></Link>
+          <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/awareness'?'acName':''}`}  onClick={() => hamburger()}>Awareness</span></li></Link>
         <Link to="/#aboutus" >
-            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/#aboutus'?'acName':''}`}>About Us</span></li></Link>
+            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/#aboutus'?'acName':''}`}  onClick={() => hamburger()}>About Us</span></li></Link>
           <Link to="/joinus">
-            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/joinus'?'acName':''}`}>Join Us</span></li></Link>
+            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/joinus'?'acName':''}`}  onClick={() => hamburger()}>Join Us</span></li></Link>
           <Link to="/contactus">
-            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/contactus'?'acName':''}`}>Contact Us</span></li></Link>
+            <li className="list-style-none mh2 black menu"><span className={` ${location.pathname==='/contactus'?'acName':''}`}  onClick={() => hamburger()}>Contact Us</span></li></Link>
           <Link to="/donatenow" className="pv2  nav-donate">
-            <a class="f5 link dim br2 ph3 pv2 white " href="" style={{ backgroundColor: "#e88f0a" }}>Donate Now</a>
+            <a class="f5 link dim br2 ph3 pv2 white " href="" style={{ backgroundColor: "#e88f0a" }}  onClick={() => hamburger()}>Donate Now</a>
           </Link>
           <p className='tc'><Link className={`f6 link dim ph3 pv1 mb2 mt2 dib white crossNavbar flex justify-center items-center`} href='#0'
             onClick={() => hamburger()}
