e5b7ba0a7c13af18154a918142c6920cbff5ec5c
Tejas Labde <labdetejas59@gmail.com>
2021-07-26 16:28:01 +0530

    events summary mapping

--- src/Components/EventsSummary/EventsSummary.js
+++ src/Components/EventsSummary/EventsSummary.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 const EventsSummary = () => {
+
+    const [ongoing, setOnGoing] = useState([]);
+    useEffect(() => {
+        fetch("https://thawing-shelf-77571.herokuapp.com/ongoingprojects")
+        .then(response => response.json())
+        .then(res => {
+          if (res[0].heading) {
+            setOnGoing(res);
+          }
+        }).catch(error => {
+          console.log(error);
+        })
+    
+      }, [])
+      
     return(
         <div id="#eventsummary" className="pt5">
-            <div className="tc f2 b mv4">Our Ongoing Projects</div>
-            <div className="flex justify-between-l items-start flex-wrap ph4-l ph2">
+            <div className="tc f2 b  mv4">Our Ongoing Projects</div>
+            {
+                ongoing.map((item,index)=>{
+                    return(
+                        <div className="flex justify-between-l items-start flex-wrap ph4-l ph2 mt4 mb4" key={item.heading}>
                 <div className="w-50-ns pa2">
-                    <h1>#hungerfreeindia</h1>
-                    <p className="tj">Tackling hunger on the frontline of emergencies is critical, but we also need long-lasting solutions. That's why we work to help people everywhere get the food they need to survive and thrive. Hunger causes a range of problems and the most severe among these is malnutrition. The World Food Programme has estimated that 1/4th of undernourished people in the world live in India. Malnutrition leads to serious health conditions such as weakness, stunted growth, anemia, and other diseases. Sharva Foundation serves the needs of people suffering from hunger and starvation by distributing Ration food kits & cooked hygienic and home-made meals for the poor and distributing the meals to the homeless, and by collecting extra food from restaurants, hotels, weddings, and other ceremonies (reducing food wastage) and distributing them to the homeless and other people in need. Till now we have distributed food to 2500 + underprivileged people.</p>
+                    <h1>{item.heading}</h1>
+                    <p className="tj"> {item.para}</p>
                 </div>
-                <div className="w-50-ns pa2">
-                    <img src="https://i.postimg.cc/BQ9kSW7d/rightimg.jpg" alt="hungerfreeindia"/>
+                <div className="w-50-ns pa2 ">
+                    <img src={item.imgUrl} alt={item.heading} className="br4" />
                 </div>
             </div>
+                    )
+                })
+            }
+            
         </div>
     );
 }
