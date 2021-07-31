3e46a921f381782c0350f0cceb365c1de742e8c9
harshika6 <harshikasmishra@gmail.com>
2021-07-31 13:51:26 +0530

    recent events done

--- src/Components/RecentEvents/RecentEvents.js
+++ src/Components/RecentEvents/RecentEvents.js
@@ -2,24 +2,40 @@ import React, {useState,useEffect} from 'react';
 import './RecentsEvents.css';
 import 'tachyons';
 function RecentEvents(){
-
- var rlength=0;
-  const [recent1, setRecent1] = useState([]);
-  const [recent2,setRecent2] = useState([]);
-  const [recent3,setRecent3] = useState([]);
+  const [recentA, setRecentA] = useState([]);
+  const [recentB, setRecentB]= useState([]);
+  const [recentC, setRecentC]= useState([]);
   useEffect(() => {
     fetch("https://thawing-shelf-77571.herokuapp.com/events")
     .then(response => response.json())
     .then(res => {
       if (res[0].tagline) {
-         // rlength= res[0].events.length;
-       console.log("hello" ,recent1);
-        setRecent1(res);
-        console.log("hello1" ,recent1);
+        setRecentA(res);
       }
     }).catch(error => {
       console.log(error);
     })
+    
+    fetch("https://thawing-shelf-77571.herokuapp.com/covid19")
+    .then(response => response.json())
+    .then(res => {
+      if (res[0].tagline) {
+        setRecentB(res);
+      }
+    }).catch(error => {
+      console.log(error);
+    })
+
+    fetch("https://thawing-shelf-77571.herokuapp.com/wings")
+    .then(response => response.json())
+    .then(res => {
+      if (res[0].tagline) {
+        setRecentC(res);
+      }
+    }).catch(error => {
+      console.log(error);
+    })
+
 
   }, [])
 
@@ -29,27 +45,78 @@ function RecentEvents(){
 		 <h1>Recent Events</h1>
        <div className="RecentEvents-container">
 
+    {     
+          recentA.map((item,index) =>{
+            console.log("item",item.events);
+            return(
+              item.events.map((i,index)=>{
+                if(index===(item.events.length-1))
+                  {                
+                    return(
+                    <div key={index} className="item  mt2 mb2 mr2 ml2 shadow-2 grow pointer" style={{background:`rgba(0, 0, 0, .6) url(${i.image[0]}) no-repeat center center`,backgroundSize:'cover', backgroundBlendMode:"darken"}}>
+                      <p className="ma3">{i.date}</p>
+                      <p className="ma3">{i.place}</p>
+                    </div>
 
-{
-      recent1.map((item,index) =>{
-         
-        return(
-          item.events.map((i,index)=>{
-            <p className="ma3">{item.date}</p>
-          })
+                    )
+                  }
+              })
 
 
+        )
+      }
+      )  
+    }
+    <div>
+      {
+        recentB.map((item,index) =>{
+          console.log("item",item.events);
+          return(
+            item.events.map((i,index)=>{
+              if(index===(item.events.length-1))
+                {                
+                  return(
+                  <div key={index} className="item  mt2 mb2 mr2 ml2 shadow-2 grow pointer" style={{background:`rgba(0, 0, 0, .6) url(${i.image[0]}) no-repeat center center`,backgroundSize:'cover', backgroundBlendMode:"darken"}}>
+                    <p className="ma3">{i.date}</p>
+                    <p className="ma3">{i.place}</p>
+                  </div>
 
-    )
-  }
-  )
-}
+                  )
+                }
+            })
+
+
+      )
+    }
+    )  
+      }
+    </div>
+    <div>
+        {
+                  recentC.map((item,index) =>{
+                    console.log("item",item.events);
+                    return(
+                      item.events.map((i,index)=>{
+                        if(index===(item.events.length-1))
+                          {                
+                            return(
+                            <div key={index} className="item  mt2 mb2 mr2 ml2 shadow-2 grow pointer" style={{background:`rgba(0, 0, 0, .6) url(${i.image[0]}) no-repeat center center`,backgroundSize:'cover', backgroundBlendMode:"darken"}}>
+                              <p className="ma3">{i.date}</p>
+                              <p className="ma3">{i.place}</p>
+                            </div>
+          
+                            )
+                          }
+                      })
+          
+          
+                )
+              }
+              ) 
+        }
+      </div>
   </div>
   </div>
   )
 }
 export default RecentEvents;
-{/* <div className="item  mt2 mb2 mr2 ml2 shadow-2 grow pointer" style={{background:`rgba(0, 0, 0, .6) url(${item.recentimg}) no-repeat center center`,backgroundSize:'cover', backgroundBlendMode:"darken"}}>
-   <h3 className="ma3">{item.heading}</h3>
-   <p className="ma3">{item.date}</p>
- </div> */}
