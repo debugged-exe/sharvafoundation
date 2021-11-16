9b76ee9e188623ff2a69efa4d683b7a93c0623f1
Tejas Labde <labdetejas59@gmail.com>
2021-11-16 22:04:17 +0530

    our events layout changed

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -168,11 +168,15 @@ function Events({initiative,setCarousel}) {
         {
           drive.map((item,index)=>{
             return(
+
+
+          
               <div className="events-1  mt5">
-                  {/* The 100% width image */}
-                  {/* 50% text and 50% image responsive div */}
-                  <div className="event-txt-img w-100">
-                      <div className="event-txt w-50-ns">
+
+
+
+                  <div className="event-txt-img ">
+                      {/* <div className="event-txt w-50-ns">
                           <h1>{item.tagline}</h1>
                           <p style={{ textAlign: "left" }}>
                             {item.description}
@@ -186,7 +190,7 @@ function Events({initiative,setCarousel}) {
                           <p style={{ textAlign: "left" }}>
                             {item.description2}
                           </p><br />
-                        {/*  <ul style={{paddingLeft:"20px"}}>
+                         <ul style={{paddingLeft:"20px"}}>
                           <li style={{ textAlign: "left" }}>
                               {item.point1}
                             </li>
@@ -205,19 +209,26 @@ function Events({initiative,setCarousel}) {
                                 <li style={{ textAlign: "left" }}>
                                     {item.point4}
                                   </li>
-                                </ul><br />*/}
+                                </ul><br />
 
+                      </div> */}
+                      {/* <div id="event-img  w-100 " >
+                      <p className="blue f3 pa1">{item.tagline}</p>
+                          <img alt="" src={item.imgright}  className="image" style={{height:"80vh",width:"100vw",objectFit:"fill"}} />
+                      </div> */}
+
+                      <div className="event-img w-100 flex justify-center items-center font-size flex-column " style={{background: `rgba(0,0,0,0.5) url(${item.imgright}) no-repeat center center`,backgroundSize:'cover',height:"80vh"}}>
+                      <h3 className="white">{item.tagline}</h3>
+                      <p className="f4 pa3 white">{item.description}</p>
                       </div>
-                      <div id="event-img  w-50-n" >
-                          <img alt="" src={item.imgright}  className="image" style={{height:"400px",width:"600px",objectFit:"contain"}} />
-                      </div>
+                      
                   </div>
 
-                  <div className="past-recent-events" >
+                  <div className="past-recent-events mt4" >
 
                       <div className="recent-events">
                           <div className="heading">
-                              <h1>RECENT EVENTS</h1>
+                              <h1>OUR EVENTS</h1>
                               <hr id="recent-hr" />
                           </div>
                           {/* Recent Events grid */}
@@ -232,7 +243,7 @@ function Events({initiative,setCarousel}) {
                                          <img alt="" src={i.image[0]} className="gridImage pointer grow shadow-5" />
                                       </Link>
                                           <p className="onrightDate f4 b">{i.place}</p>
-                                          <p className="onrightDate">{i.date}</p>
+                                          {/* <p className="onrightDate">{i.date}</p> */}
 
                                     </div>
 
@@ -244,7 +255,7 @@ function Events({initiative,setCarousel}) {
 
                           </div>
                       </div>
-                      <div className="past-events">
+                      {/* <div className="past-events">
                        {
                          abc.map((a,index)=>{
                            if(abc.length<0){
@@ -286,9 +297,9 @@ function Events({initiative,setCarousel}) {
                             }
 
                           </div>
-                      </div>
+                      </div> */}
                   </div>
-                  <div>
+                  {/* <div>
                       {
                           showP ? drive : null
                       }
@@ -316,7 +327,7 @@ function Events({initiative,setCarousel}) {
                       }
 
                       </div>
-                  </div>
+                  </div> */}
               </div >
 
 
